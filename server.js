dcc0350a64c3bcea007de48da1013569bcfb7e38
Shane Hartman <shane@shanehartman.com>
2017-12-30 14:59:28 -0600

    update from upstream

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ var requestHandler = function (req, res) {
       }
 
       if (req.method === 'GET') {
-        console.log(req.url);
         api.requestHandler(req, res);
       }
     });
@@ -65,9 +64,6 @@ if (settings.https) {
   } else if (settings.https.key && settings.https.cert) {
     options.key = fs.readFileSync(settings.https.key);
     options.cert = fs.readFileSync(settings.https.cert);
-    if (settings.https.ca) {
-       options.ca = fs.readFileSync(settings.https.ca);
-    }
   } else {
     logger.error("Insufficient configuration for https");
     return;
@@ -85,8 +81,9 @@ process.on('unhandledRejection', (err) => {
   logger.error(err);
 });
 
-server.listen(settings.port, function () {
-  logger.info('http server listening on port', settings.port);
+let host = settings.ip;
+server.listen(settings.port, host, function () {
+  logger.info('http server listening on', host, 'port', settings.port);
 });
 
 server.on('error', (err) => {
