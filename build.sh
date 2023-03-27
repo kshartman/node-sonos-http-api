220b4aa5507b789c3d79b8e9620eb6c4a220ab90
Shane Hartman <shane@shanehartman.com>
2023-03-27 01:29:05 -0500

    update build to copy settings for target

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+if (( $# >= 1 )); then
+   BUILDFOR=$1
+else
+    BUILDFOR=$(echo "${HOSTNAME%%.*}" | tr '[:upper:]' '[:lower:]')
+fi    
+
+if [ -f ../private/settings-${BUILDFOR}.json ]; then
+    rm settings.json
+    cp ../private/settings-${BUILDFOR}.json settings.json
+    rm settings-${BUILDFOR}.json
+    ln -s ../private/settings-${BUILDFOR}.json .
+else
+    echo error: no such settings ../private/settings-${BUILDFOR}.json
+    exit 1
+fi
 
 # image name
 IMAGE=sonosd
